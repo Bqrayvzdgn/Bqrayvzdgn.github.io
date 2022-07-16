8cb6f0a978c6dca066bc0e28427328a501e40475
Bqrayvzdgn <bqrayvzdgn74@gmail.com>
2022-07-16 23:05:42 +0300

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".btn").fadeIn();
-        }
-        else {
-            $(".btn").fadeOut();
-        }
-    });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $(".btn").fadeIn();
+    } else {
+      $(".btn").fadeOut();
+    }
+  });
 
-    $(".btn").click(function () {
-        $("html,body").animate({ scrollTop: 0 }, 700);
-        return false;
-    });
+  $(".btn").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 700);
+    return false;
+  });
 });
